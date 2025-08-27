52fa57a7545cfa3565d5d9a19843d6c2798782f0
Hannah <Hannah2520@gmail.com>
2025-08-27 14:49:02 -0400

    new assets

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
-<script setup></script>
+<script setup>
+  import './assets/styles.css';
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+<p>hi</p>
 </template>
+
+
 
 <style scoped></style>
